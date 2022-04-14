c63c9907f3ad6a1ff5896b388bb3f7c2eb22490c
Svdex <ucihaibna@yahoo.com>
2022-04-15 05:28:53 +0700

    Add api for getAccount

---
+++ pages/api/account/index.js
@@ -0,0 +1,18 @@
+import mysql_con from "../../../lib/mysl_con";
+
+import { withIronSessionApiRoute } from "iron-session/next";
+import { ironSessionConfig } from "../../../next.config";
+
+export default withIronSessionApiRoute(accountRoute, ironSessionConfig);
+
+function accountRoute(req, res) {
+  if (!req.session.user || !req.session.user.admin)
+    res.status(404).send("Not found.");
+  if (req.method != "GET") {
+    res.status(405).json({ error: true, message: "Only GET.", data: [] });
+    return;
+  }
+  mysql_con.getDataAkun((_res) => {
+    res.status(200).json({ error: false, message: "ok", data: _res });
+  });
+}

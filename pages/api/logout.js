944d5f631655e71c6609fff8a641d37b567e3f8b
Svdex <ucihaibna@yahoo.com>
2022-05-24 15:16:42 +0700

    Fix session destroy on logout

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,8 +1,13 @@
 import { withIronSessionApiRoute } from "iron-session/next";
+import mysql_con from "../../lib/mysl_con";
 import { ironSessionConfig } from "../../next.config";
 
 export default withIronSessionApiRoute(async (req, res) => {
   const user = req.session.user;
-  if (user) await req.session.destroy();
+
+  if (user) {
+    mysql_con.deleteSession(req.session.user.nomor_hp, () => {});
+    req.session.destroy();
+  }
   res.redirect("/");
 }, ironSessionConfig);
